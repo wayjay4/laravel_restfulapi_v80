59407697cc56e7ce90bfd39a87860dd541018848
vagrant <vagrant@homestead>
2020-06-28 03:38:37 +0000

    modified "mass fillable" for Category and Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id',
+    ];
 }
