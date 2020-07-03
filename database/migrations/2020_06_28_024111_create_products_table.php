02c312818faa8f3206062052c4088fa1e831bc06
vagrant <vagrant@homestead>
2020-07-03 00:11:42 +0000

    fixed foreign key constraints

--- database/migrations/2020_06_28_024111_create_products_table.php
+++ database/migrations/2020_06_28_024111_create_products_table.php
@@ -21,7 +21,7 @@ class CreateProductsTable extends Migration
             $table->integer('quantity')->unsigned();
             $table->string('status')->default(Product::UNAVAILABLE_PRODUCT);
             $table->string('image');
-            $table->integer('seller_id')->unsigned();
+            $table->unsignedBigInteger('seller_id');
             $table->timestamps();
 
             $table->foreign('seller_id')->references('id')->on('users');
