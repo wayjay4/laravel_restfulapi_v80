809e28f643778849c0cb5dc65584500d3fb72c17
Waylon Dixon <wayjay4@yahoo.com>
2020-07-13 02:29:53 +0000

    traits: defined and used generalized response method for success and errors

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Buyer;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use App\Buyer;
 
-class BuyerController extends Controller
+class BuyerController extends ApiController
 {
     /**
      * Display a listing of the resource.
@@ -17,7 +17,7 @@ class BuyerController extends Controller
     {
         $buyers = Buyer::has('transactions')->get();
 
-        return response()->json(['data' => $buyers], 200);
+        return $this->showAll($buyers);
     }
 
     /**
@@ -28,8 +28,8 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        $buyers = Buyer::has('transactions')->findOrFail($id);
+        $buyer = Buyer::has('transactions')->findOrFail($id);
 
-        return response()->json(['data' => $buyers], 200);
+        return $this->showOne($buyer);
     }
 }
