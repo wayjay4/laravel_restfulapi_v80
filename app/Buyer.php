ea22c3d1c3714762932ef43c6776890e3c68d5e1
Waylon Dixon <wayjay4@yahoo.com>
2020-07-13 00:20:44 +0000

    implemented api operations for BuyerController

--- app/Buyer.php
+++ app/Buyer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Buyer extends User
 {
     // model relation between User and Transactions
-    public function transations(){
+    public function transactions(){
     	return $this->hasMany(Transaction::class);
     }
 }
