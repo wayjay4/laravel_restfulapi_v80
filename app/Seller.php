974d8907b0a524a17b65610318a243268f7f1125
Waylon Dixon <wayjay4@yahoo.com>
2020-07-13 19:08:07 +0000

    resolving seller using laravel global scopes

--- app/Seller.php
+++ app/Seller.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+	protected static function boot(){
+		parent::boot();
+
+		static::addGlobalScope(new SellerScope);
+	}
+
     public function products(){
     	return $this->hasMany(Product::class);
     }
