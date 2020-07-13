61fa482bbe163083f2fb1e7df84e793e9b47c1cb
Waylon Dixon <wayjay4@yahoo.com>
2020-07-13 18:57:58 +0000

    resolving buyer using laravel global scopes

--- app/Buyer.php
+++ app/Buyer.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
+	protected static function boot(){
+		parent::boot();
+
+		static::addGlobalScope(new BuyerScope);
+	}
+
     // model relation between User and Transactions
     public function transactions(){
     	return $this->hasMany(Transaction::class);
