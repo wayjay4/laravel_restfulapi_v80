45d9006cb905f685bef4239f3048d684e7dbca63
Waylon Dixon <wayjay4@yahoo.com>
2020-07-14 00:48:24 +0000

    correction: implemented complex nested eagar loading operation for index method of BuyerSellerController

--- app/Http/Controllers/Buyer/BuyerSellerController.php
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -16,7 +16,11 @@ class BuyerSellerController extends ApiController
     public function index(Buyer $buyer)
     {
 
-        $seller = $buyer->transactions()->with('product.seller')->get()->pluck('product.seller');
+        $seller = $buyer->transactions()->with('product.seller')
+        ->get()
+        ->pluck('product.seller')
+        ->unique('id')
+        ->values();
 
         return $this->showAll($seller);
     }
