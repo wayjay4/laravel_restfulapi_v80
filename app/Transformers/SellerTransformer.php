e8ed5cc295a9efe142039b0b97c3174013d2060c
Waylon Dixon <wayjay4@yahoo.com>
2020-07-26 23:38:05 +0000

    forgot to save the seller transformer

--- app/Transformers/SellerTransformer.php
+++ app/Transformers/SellerTransformer.php
@@ -33,13 +33,13 @@ class SellerTransformer extends TransformerAbstract
     public function transform(Seller $seller)
     {
         return [
-            'identifier' => (int) $buyer->id,
-            'name' =>(string) $buyer->name,
-            'email' => (string) $buyer->email,
-            'isVerified' => (int) $buyer->verified,
-            'creationDate' => $buyer->created_at,
-            'lastChange' => $buyer->updated_at,
-            'deletedDate' => isset($buyer->deleted_at) ? (string) $buyer->deleted_at : null,
+            'identifier' => (int) $seller->id,
+            'name' =>(string) $seller->name,
+            'email' => (string) $seller->email,
+            'isVerified' => (int) $seller->verified,
+            'creationDate' => $seller->created_at,
+            'lastChange' => $seller->updated_at,
+            'deletedDate' => isset($seller->deleted_at) ? (string) $seller->deleted_at : null,
         ];
     }
 }
