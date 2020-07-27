e1cbd4cdceef8d5d06a19f433447860074827318
Waylon Dixon <wayjay4@yahoo.com>
2020-07-27 23:20:42 +0000

    ran composer update/install, installed ui react with authentication

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
