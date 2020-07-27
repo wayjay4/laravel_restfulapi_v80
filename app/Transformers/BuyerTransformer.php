8d5a99c1e19013c9e7c951bb864e4f43d5afe56c
Waylon Dixon <wayjay4@yahoo.com>
2020-07-27 02:45:56 +0000

    modified model transformations to solve the sorting problem

--- app/Transformers/BuyerTransformer.php
+++ app/Transformers/BuyerTransformer.php
@@ -34,12 +34,26 @@ class BuyerTransformer extends TransformerAbstract
     {
         return [
             'identifier' => (int) $buyer->id,
-            'name' =>(string) $buyer->name,
+            'name' => (string) $buyer->name,
             'email' => (string) $buyer->email,
             'isVerified' => (int) $buyer->verified,
             'creationDate' => (string) $buyer->created_at,
             'lastChange' => (string) $buyer->updated_at,
             'deletedDate' => isset($buyer->deleted_at) ? (string) $buyer->deleted_at : null,
         ];
+    }
+
+    public static function originalAttribute($index){
+        $attributes = [
+            'identifier' => 'id',
+            'name' => 'name',
+            'email' => 'email',
+            'isVerified' => 'verified',
+            'creationDate' => 'created_at',
+            'lastChange' => 'updated_at',
+            'deletedDate' => 'deleted_at',
+        ];
+
+        return isset($attributes[$index]) ? $attributes[$index] : null;
     }
 }
