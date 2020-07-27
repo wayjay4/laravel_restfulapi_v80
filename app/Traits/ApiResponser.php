def07ac8cf6a69bc862af652c8e199674f2cb63a
Waylon Dixon <wayjay4@yahoo.com>
2020-07-27 04:38:47 +0000

    paginating already built collections

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,8 +2,9 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 trait ApiResponser{
 	private function successResponse($data, $code){
@@ -24,6 +25,8 @@ trait ApiResponser{
 		$collection = $this->filterData($collection, $transformer);
 
 		$collection = $this->sortData($collection, $transformer);
+
+		$collection = $this->paginate($collection);
 
 		$collection = $this->transformData($collection, $transformer);
 
@@ -58,6 +61,22 @@ trait ApiResponser{
 		}
 
 		return $collection;
+	}
+
+	protected function paginate(Collection $collection){
+		$page = LengthAwarePaginator::resolveCurrentPage();
+
+		$perPage = 3;
+
+		$results = $collection->slice(($page-1)*$perPage, $perPage)->values();
+
+		$paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+			'path' => LengthAwarePaginator::resolveCurrentPath(),
+		]);
+
+		$paginated->appends(request()->all());
+
+		return $paginated;
 	}
 
 	protected function transformData($data, $transformer){
