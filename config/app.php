c00b1c13f17558092737d907a95f0a3e1cc80e38
Waylon Dixon <wayjay4@yahoo.com>
2020-07-28 03:28:32 +0000

    install and prepare project to use laravel passport

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Fractal\FractalServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
