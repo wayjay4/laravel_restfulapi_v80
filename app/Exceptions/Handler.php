0feacc47d94f17ece4543d402aceefed7d4030f8
Waylon Dixon <wayjay4@yahoo.com>
2020-07-28 01:18:40 +0000

    added CSRF Exception handling, redirects user back to login rather than display token error message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,17 +2,18 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
-use Illuminate\Validation\ValidationException;
 use App\Traits\ApiResponser;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Access\AuthorizationException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -97,6 +98,10 @@ class Handler extends ExceptionHandler
             if($errorCode == 1451){
                 return $this->errorResponse('Cannont remove this resource permanently. It is related with any other resource.', 409);
             }
+        }
+
+        if($exception instanceof TokenMismatchException){
+            return redirect()->back()->withInput($request->input());
         }
 
         if(config('app.debug')){
