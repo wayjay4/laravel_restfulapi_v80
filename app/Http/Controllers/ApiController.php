41a5615fcdb44d0ba464c016642d37ccd83b56f7
Waylon Dixon <wayjay4@yahoo.com>
2020-07-29 04:36:00 +0000

    protecting all remaining routes with auth:api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,7 +11,7 @@ class ApiController extends Controller
 
     public function __construct()
     {
-    	//
+    	$this->middleware('auth:api');
     }
 
 }
--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -13,6 +13,8 @@ class UserController extends ApiController
     {
         $this->middleware('client.credentials')->only(['store', 'resend']);
 
+        $this->middleware('auth:api')->except(['store', 'verify', 'resend']);
+
         $this->middleware('transform.input:'.UserTransformer::class)->only(['store', 'update']);
     }
 
