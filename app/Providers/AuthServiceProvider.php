d5eb0dc01793ee16114433962595bc2ccd6c43ff
Waylon Dixon <wayjay4@yahoo.com>
2020-09-11 06:04:05 +0000

    registered some required scopes for the api and middleware to check scopes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -34,5 +34,12 @@ class AuthServiceProvider extends ServiceProvider
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
 
         Passport::enableImplicitGrant();
+
+        Passport::tokensCan([
+          'purchase-product' => 'Create a new transaction for a specific product.',
+          'manage-products' => 'Create, read, update, and delete products (CRUD)',
+          'manage-account' => 'Read your account data, id, name, email, if verified, and if admin (cannot read password). Cannot delete your account.',
+          'read-general' => 'Read general information like purchasing categories, purchased products, selling products, selling categories, your transactions (purchases and sales)',
+        ]);
     }
 }
