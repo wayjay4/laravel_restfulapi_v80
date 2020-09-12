f491a4ceb61514b9ef41b3bf65f7074f669a188c
Waylon Dixon <wayjay4@yahoo.com>
2020-09-12 04:45:13 +0000

    restricted the actions that need the manage-products scope

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -14,6 +14,8 @@ class ProductCategoryController extends ApiController
         $this->middleware('client.credentials')->only(['index']);
 
         $this->middleware('auth:api')->except(['index']);
+
+        $this->middleware('scope:manage-products')->except(['index']);
     }
 
     /**
