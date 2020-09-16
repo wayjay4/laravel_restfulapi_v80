2a9d8aa1f170b0eb4779fbb9afcce5d391899d3d
Waylon Dixon <wayjay4@yahoo.com>
2020-09-16 04:42:08 +0000

    created and implemented 'email-updated' mailable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Product;
 use App\Mail\UserCreated;
 use Illuminate\Http\Request;
+use App\Mail\UserMailChanged;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -34,8 +35,16 @@ class AppServiceProvider extends ServiceProvider
         User::created(function($user){
             // check if request is coming from an API request (i.e. not from frontend)
             // if so, then send email welcome/email-verification
-            if(collect($this->app->request->route()->middleware())->contains('api')){
+            if($this->isApiRequest()){
                 Mail::to($user)->send(new UserCreated($user));
+            }
+        });
+
+        User::updated(function($user){
+            // check if request is coming from an API request (i.e. not from frontend) and email was 'changed'
+            // if so, then send email welcome/email-verification
+            if($user->isDirty('email') && $this->isApiRequest()){
+                Mail::to($user)->send(new UserMailChanged($user));
             }
         });
 
@@ -46,5 +55,10 @@ class AppServiceProvider extends ServiceProvider
                 $product->save();
             }
         });
+    }
+
+    private function isApiRequest()
+    {
+        return collect($this->app->request->route()->middleware())->contains('api');
     }
 }
