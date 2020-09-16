bc0b17dd6b64970dc90a66f4bb0a808d5f26ac07
Waylon Dixon <wayjay4@yahoo.com>
2020-09-16 15:15:57 +0000

    dealing with failing-prone email actions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,10 @@ class AppServiceProvider extends ServiceProvider
             // check if request is coming from an API request (i.e. not from frontend)
             // if so, then send email welcome/email-verification
             if($this->isApiRequest()){
-                Mail::to($user)->send(new UserCreated($user));
+                // send the email using retry for 5 times every 100ms if failed
+                retry(5, function() use($user) {
+                    Mail::to($user)->send(new UserCreated($user));
+                }, 100);
             }
         });
 
@@ -44,7 +47,10 @@ class AppServiceProvider extends ServiceProvider
             // check if request is coming from an API request (i.e. not from frontend) and email was 'changed'
             // if so, then send email welcome/email-verification
             if($user->isDirty('email') && $this->isApiRequest()){
-                Mail::to($user)->send(new UserMailChanged($user));
+                // send the email using retry for 5 times every 100ms if failed
+                retry(5, function() use($user) {
+                    Mail::to($user)->send(new UserMailChanged($user));
+                }, 100);
             }
         });
 
